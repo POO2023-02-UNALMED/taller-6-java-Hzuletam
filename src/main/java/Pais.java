31e993d037b9ea667632e772e4645ab522d23776
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-10-05 14:10:25 -0500

    a

--- src/main/java/Pais.java
+++ src/main/java/Pais.java
@@ -1,20 +1,11 @@
 package vehiculos;
 
-import java.util.Map;
-import java.util.HashTable;
-import java.util.Set;
-
 class Pais{
     String nombre;
-    static Map<String , int> paises = new HashTable();
 
     public Pais(String nombre){
         this.nombre = nombre ;
-        if (paises.contains(nombre) == false ){
-            paises.put(nombre,(int)0);
-        }
     }
-
     public static void unaVentaMas(String nombre){
         paises.put(nombre, paises.get(nombre) + 1);
     }
@@ -31,8 +22,3 @@ class Pais{
         }
         return paisMasVendedor;
     }
-
-    public void unaVentaMas(String nombre){
-        paises.put(nombre,paises.get(nombre) + 1);
-    }
-}
