eea8d1bdbfbcac4f7e5121cd9d2bf18282922f09
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-10-05 13:14:39 -0500

    Update Fabricante.java
    creacion metodo mejorfabricante

--- src/main/java/Fabricante.java
+++ src/main/java/Fabricante.java
@@ -2,15 +2,38 @@ package vehiculos;
 
 import Pais;
 
+import java.util.Map;
+import java.util.HashTable;
+import java.util.Set;
+
 public class Fabricante{
-    String nombre ;
-    Pais pais;
+    public String nombre ;
+    public Pais pais;
+    public Map<String, int> fabricas = new HashTable();
 
     public Fabricante(Pais pais, String nombre){
         this.nombre = nombre ; 
         this.pais = pais;
+        if (fabricas.contains(nombre) = false){
+            fabricas.put(nombre, (int) 0);
+        }
         
     }
 
-    public String fabricaMayorVentas()
+    public void unaVentaMas(String nombre){
+        fabricas.put(nombre,fabricas.get(nombre) + 1);
+    }
+    
+    public String fabricaMayorVentas(){
+    Set<String> fabricas = fabricas.keyset();
+    String mejorFabrica;
+    int cantidadVendidos;
+    for(String fabrica : fabricas){
+    if (fabricas.get(fabrica) > cantidadVendidos){
+        mejorFabrica = fabrica;
+        cantidadVendidos = fabricas.get(fabrica);
+        }
+    return mejorFabrica;
+        }
+    }
 }
