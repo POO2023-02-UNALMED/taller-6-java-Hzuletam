ecaee449bdea29b5cb3860643821a99e01b9fdcc
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-10-05 22:26:47 -0500

    ab

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -7,6 +7,7 @@ public class Camion extends Vehiculo{
     private static ArrayList<Camion> cantidadCamiones = new ArrayList<>();
 
     public Camion(){
+        cantidadCamiones.add(this);
         
 
     }
@@ -14,7 +15,7 @@ public class Camion extends Vehiculo{
     public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes){
         super(placa ,2, 80,nombre, precio, peso, "4X2",fabricante);
         this.ejes = ejes;
-        
+        cantidadCamiones.add(this);
     }
 
     public int getEjes(){
